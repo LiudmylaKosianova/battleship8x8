f2c4b578ee11fb021b191a8722f8a97019946f72
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-23 18:45:14 +0200

    finished shoot, but not sure

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -9,7 +9,44 @@ public class Battleship8x8 {
     }
 
     public boolean shoot(String shot) {
-        throw new UnsupportedOperationException();
+        //turn ships to binary so that it represents the map with ships location
+        String map = Long.toBinaryString(ships);
+
+
+        //my aim is to create a two-dimensional array to represent map with ships
+        String[] onlyRows = new String[8];
+        for (int i = 0; i < 8; i++) {
+            onlyRows[i] = map.substring(i * 8, (i * 8) + 8);
+        }
+
+        String[][] battleMap = new String[8][8];
+        for(int x = 0; x<8; x++){
+            battleMap[x] = onlyRows[x].split("");
+        }
+
+        //check if there is a ship at the location, represented by a "shot"
+        int row = ((int) shot.charAt(1)) - 1;
+        char columnLetter = shot.charAt(0);
+        int column = 0;
+        if(columnLetter == 'A'){
+            column = 0;
+        }else if(columnLetter == 'B'){
+            column = 1;
+        }else if(columnLetter == 'C'){
+            column = 2;
+        }else if(columnLetter == 'D'){
+            column = 3;
+        }else if(columnLetter == 'E'){
+            column = 4;
+        }else if(columnLetter == 'F'){
+            column = 5;
+        }else if(columnLetter == 'G'){
+            column = 6;
+        }else if(columnLetter == 'H'){
+            column = 7;
+        }
+
+        return "1".equals(battleMap[row][column]);
     }
 
     public String state() {
