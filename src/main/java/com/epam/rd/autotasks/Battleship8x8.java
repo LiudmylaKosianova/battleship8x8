7541a0c04c3ecba28c1245cd25cfbb1bbdcbf69c
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-24 11:52:03 +0200

    failed 3 tests, best result so far

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -10,17 +10,33 @@ public class Battleship8x8 {
 
     public boolean shoot(String shot) {
         String shipsMap = Long.toBinaryString(ships);
+
         int index = shotToIndex(shot);
 
         //I need to register the shot
         StringBuilder shotsMap = new StringBuilder();
+
+        //convert long "shots" to string of 0s and 1s
         if (shots != 0){
             shotsMap = new StringBuilder(Long.toBinaryString(shots));
+
         }else{
-            shotsMap.append("0".repeat(63));
+            shotsMap.append("0".repeat(index+1));
         }
-        shotsMap.setCharAt(index,'1');
+
+        //put '1' into the place of current shot
+        if(index<shotsMap.length()){
+            shotsMap.setCharAt(index,'1');
+        }else {
+            for(int i=shotsMap.length(); i<=index; i++){
+                shotsMap.append('0');
+            }
+            shotsMap.setCharAt(index,'1');
+        }
+
+        //turn string shotsMap into long integer again
         shots = Long.parseLong(shotsMap.toString(),2);
+
 
         return shipsMap.charAt(index) == '1';
     }
@@ -60,14 +76,47 @@ public class Battleship8x8 {
     }
     public String state() {
         //throw new UnsupportedOperationException();
-         return "not yet";
+        String shipsMap = Long.toBinaryString(ships);
+
+        StringBuilder shotsMap = new StringBuilder(Long.toBinaryString(shots));
+        if(shotsMap.length()<64){
+            for(int i = shotsMap.length(); i<64; i++){
+                shotsMap.append('0');
+            }
+        }
+
+
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i<64; i++){
+
+            if(shipsMap.charAt(i)== '0' && shotsMap.charAt(i) == '0'){
+                answer.append('.');
+            }else if( shipsMap.charAt(i)== '0' && shotsMap.charAt(i) == '1'){
+                answer.append('×');
+            }else if(shipsMap.charAt(i)== '1' && shotsMap.charAt(i) == '0'){
+                answer.append('☐');
+            }else if(shipsMap.charAt(i)== '1' && shotsMap.charAt(i) == '1'){
+                answer.append('☒');
+            }
+        }
+        answer.insert(8,"\n");
+        answer.insert(17, "\n");
+        answer.insert(26,"\n");
+        answer.insert(35,"\n");
+        answer.insert(44,"\n");
+        answer.insert(53,"\n");
+        answer.insert(62,"\n");
+
+
+         return answer.toString();
     }
 
     public static void main(String[] args) {
         Battleship8x8 myTry = new Battleship8x8(-1150950973573693440L);
-        System.out.println(myTry.shoot("B1"));
-        //myTry.shoot("A1");
-        //myTry.shoot("F3");
+        System.out.println(myTry.shoot("A1"));
+        myTry.shoot("D1");
+        myTry.shoot("H1");
         System.out.println(myTry.shoot("A2"));
+        System.out.println(myTry.state());
     }
 }
