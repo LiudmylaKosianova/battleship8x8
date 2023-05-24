ff9d5cbb07d3094781d7d8cf21409d5e14f95990
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-24 08:55:24 +0200

    shoot changed, but not finished

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -9,106 +9,55 @@ public class Battleship8x8 {
     }
 
     public boolean shoot(String shot) {
+        String shipsMap = Long.toBinaryString(ships);
+        int index = shotToIndex(shot);
 
-        //create a map of a battleship field
-        String [][] battleMap = longToBattlemap(ships);
-
-        //convert the "shot" into location on the map
-        String rowAndColumn = shotTolocation(shot);
-        int row = (int) rowAndColumn.charAt(0) - '0';
-        int column = (int) rowAndColumn.charAt(1) - '0';
-
-        //I need to register a shot
-        String[][]shotsMap = new String[8][8];
-        if(shots ==0){
-            for(int x=0; x<8; x++){
-                for(int y=0; y<8; y++){
-                    shotsMap[x][y] = "0";
-                }
-            }
-        }else{
-            shotsMap = longToBattlemap(shots);
-        }
-        shotsMap[row][column] = "1";
-        shots = battlemapToLong(shotsMap);
-
-
-        //check if there is a ship at the location, represented by a "shot"
-        return "1".equals(battleMap[row][column]);
+        return shipsMap.charAt(index) == '1';
     }
-
-    /*
-    * takes a long integer as an argument
-    * turns it into binary literal
-    * then turns the binary into a two-dimentional array 8x8
-    */
-    private String[][] longToBattlemap (long longN){
-        //long into binary
-        String map = Long.toBinaryString(longN);
-
-        //one-dimentional array representing only 8 rows
-        String[] onlyRows = new String[8];
-        for (int i = 0; i < 8; i++) {
-            onlyRows[i] = map.substring(i * 8, (i * 8) + 8);
-        }
-
-        //two-dimentional array 8x8
-        String[][] battleMap = new String[8][8];
-        for(int x = 0; x<8; x++){
-            battleMap[x] = onlyRows[x].split("");
-        }
-
-        return battleMap;
-    }
-
-    private long battlemapToLong (String[][] map){
-        String binaryLong = "";
-        for(int x=0; x<8; x++){
-            for(int y=0; y<8; y++){
-                binaryLong += map[x][y];
-            }
-        }
-        return Long.parseLong(binaryLong,2);
-    }
-
 
 /*
 * takes a String "shot" of the form eg. "A2"
-* and converts it into a String "01"
-* where "0" and "1" are indexes of the location
-* of a point on a battle map
+* and converts it into an int, representing
+* the index
 */
-    private String shotTolocation (String shot){
-        int row = shot.charAt(1) - '0' - 1;
-        char columnLetter = shot.charAt(0);
+    private int shotToIndex (String shot){
+
+        char letter = shot.charAt(0);
         int column = 0;
-        if(columnLetter == 'A'){
+        if(letter == 'A'){
             column = 0;
-        }else if(columnLetter == 'B'){
+        }else if(letter == 'B'){
             column = 1;
-        }else if(columnLetter == 'C'){
+        }else if(letter == 'C'){
             column = 2;
-        }else if(columnLetter == 'D'){
+        }else if(letter == 'D'){
             column = 3;
-        }else if(columnLetter == 'E'){
+        }else if(letter == 'E'){
             column = 4;
-        }else if(columnLetter == 'F'){
+        }else if(letter == 'F'){
             column = 5;
-        }else if(columnLetter == 'G'){
+        }else if(letter == 'G'){
             column = 6;
-        }else if(columnLetter == 'H'){
+        }else if(letter == 'H'){
             column = 7;
         }
-        return String.valueOf(row) + String.valueOf(column);
+
+        String number = String.valueOf(shot.charAt(1));
+        int row = Integer.valueOf(number);
+
+        return (8 * (row -1)) + column;
 
     }
     public String state() {
         //throw new UnsupportedOperationException();
-        return "not done yet";
+         return "not yet";
     }
 
     public static void main(String[] args) {
         Battleship8x8 myTry = new Battleship8x8(-1150950973573693440L);
         System.out.println(myTry.shoot("B1"));
+        //myTry.shoot("A1");
+        //myTry.shoot("F3");
+        System.out.println(myTry.shoot("A1"));
     }
 }
