879219794a9fcee7bd88c9329408e4d2ea71e2d1
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-24 13:57:50 +0200

    failed 2 tests, best result so far

--- src/main/java/com/epam/rd/autotasks/Battleship8x8.java
+++ src/main/java/com/epam/rd/autotasks/Battleship8x8.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autotasks;
 
+import java.math.BigInteger;
+
 public class Battleship8x8 {
     private final long ships;
     private long shots = 0L;
@@ -9,33 +11,25 @@ public class Battleship8x8 {
     }
 
     public boolean shoot(String shot) {
-        String shipsMap = Long.toBinaryString(ships);
+        StringBuilder shipsMap = new StringBuilder( Long.toBinaryString(ships) );
+
+        if(shipsMap.length() < 64){
+            shipsMap.insert(0,"0");
+        }
 
         int index = shotToIndex(shot);
 
         //I need to register the shot
-        StringBuilder shotsMap = new StringBuilder();
-
-        //convert long "shots" to string of 0s and 1s
-        if (shots != 0){
-            shotsMap = new StringBuilder(Long.toBinaryString(shots));
-
-        }else{
+        if(shots == 0){
+            StringBuilder shotsMap = new StringBuilder();
             shotsMap.append("0".repeat(index+1));
+            shotsMap.setCharAt(index,'1');
+            BigInteger shotsMapB = new BigInteger(shotsMap.toString(),2);
+            shots = shotsMapB.longValue();
+        }else{
+            registerTheShot(shot);
         }
 
-        //put '1' into the place of current shot
-        if(index<shotsMap.length()){
-            shotsMap.setCharAt(index,'1');
-        }else {
-            for(int i=shotsMap.length(); i<=index; i++){
-                shotsMap.append('0');
-            }
-            shotsMap.setCharAt(index,'1');
-        }
-
-        //turn string shotsMap into long integer again
-        shots = Long.parseLong(shotsMap.toString(),2);
 
 
         return shipsMap.charAt(index) == '1';
@@ -69,14 +63,42 @@ public class Battleship8x8 {
         }
 
         String number = String.valueOf(shot.charAt(1));
-        int row = Integer.valueOf(number);
+        int row = Integer.parseInt(number);
 
         return (8 * (row -1)) + column;
 
     }
+
+    private void registerTheShot (String shot){
+        int index = shotToIndex(shot);//I will need to put '1' into this location
+
+        //convert long integer "shots" to a string representing its value in binary
+        StringBuilder shotsMap = new StringBuilder(Long.toBinaryString(shots));
+
+        //I will put '1' into the location "index"
+        if(shotsMap.length()>index){
+            shotsMap.setCharAt(index,'1');
+        }else if(shotsMap.length()==index){
+            shotsMap.append('1');
+        }else {
+            for(int i = shotsMap.length(); i<index; i++){
+                shotsMap.append('0');
+            }
+            shotsMap.append('1');
+        }
+        BigInteger shotsMapB = new BigInteger(shotsMap.toString(), 2);
+        shots = shotsMapB.longValue();
+
+
+    }
     public String state() {
-        //throw new UnsupportedOperationException();
-        String shipsMap = Long.toBinaryString(ships);
+
+        //String shipsMap = Long.toBinaryString(ships);
+        StringBuilder shipsMap = new StringBuilder( Long.toBinaryString(ships) );
+
+        if(shipsMap.length() < 64){
+            shipsMap.insert(0,"0");
+        }
 
         StringBuilder shotsMap = new StringBuilder(Long.toBinaryString(shots));
         if(shotsMap.length()<64){
@@ -112,11 +134,19 @@ public class Battleship8x8 {
     }
 
     public static void main(String[] args) {
-        Battleship8x8 myTry = new Battleship8x8(-1150950973573693440L);
-        System.out.println(myTry.shoot("A1"));
-        myTry.shoot("D1");
-        myTry.shoot("H1");
-        System.out.println(myTry.shoot("A2"));
+        Battleship8x8 myTry = new Battleship8x8(0b01010000_01000010_01000100_01000000_00001110_00110001_10000100_11100000L);
+        System.out.println(myTry.shoot("A3"));
+        myTry.shoot("D7");
+        myTry.shoot("A5");
+        myTry.shoot("F3");
+        myTry.shoot("H8");
+
+        myTry.shoot("D3");
+        myTry.shoot("B3");
+        myTry.shoot("G8");
+        myTry.shoot("C3");
+
         System.out.println(myTry.state());
+
     }
 }
